91f782a99461d36dcb7c93d4211c6e40a797f3ca
ayrton <ayrtonobraam@gmail.com>
2017-01-24 17:31:36 +0100

    added new lines

--- SRC_Folder/src/Main.java
+++ SRC_Folder/src/Main.java
@@ -1,8 +1,7 @@
-/**
- * Created by ayrton on 24-1-2017.
- */
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("How are you?");
     }
 }
